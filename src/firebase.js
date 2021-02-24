951d75a40b525209072d173ec08a8b52e4b3729f
bedirhannbayrak <bedirhan.bayrak41@gmail.com>
2021-02-24 14:49:55 +0300

    5- firebase and react redux installed

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBobDxZnZQS8MF_pOki4zKsOF3HZnyC-Kw",
+    authDomain: "chatify-5c1be.firebaseapp.com",
+    projectId: "chatify-5c1be",
+    storageBucket: "chatify-5c1be.appspot.com",
+    messagingSenderId: "1034600893137",
+    appId: "1:1034600893137:web:5ba1c69c53d98f498f13f0",
+    measurementId: "G-LJXX28EPXV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebase;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from './firebase';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
